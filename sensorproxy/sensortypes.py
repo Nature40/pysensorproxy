9afda87e45321c1d8cc0abda3fd965a9b4893ba3
Alvar Penning <post@0x21.biz>
2019-02-11 16:29:59 +0100

    Record a microphone

--- sensorproxy/sensortypes.py
+++ sensorproxy/sensortypes.py
@@ -1,12 +1,13 @@
-import time
-import os
 import errno
+import os
+import subprocess
+import time
 
-from typing import Dict, Type
 from abc import ABC, abstractmethod
+from typing import Dict, Type
 
-import random
 import Adafruit_DHT
+import random
 
 
 class Sensor:
@@ -19,11 +20,13 @@ class Sensor:
     def read(self):
         pass
 
+
 # classes: Dict[str, Type[Sensor]] = {}
 classes = {}
 def _register_sensor(cls: Type[Sensor]):
     classes[cls.__name__] = cls
     return cls
+
 
 class LogSensor(Sensor):
     def __init__(self, *args, **kwargs):
@@ -31,6 +34,7 @@ class LogSensor(Sensor):
 
         self.file_path = os.path.join(self.storage_path, "{}_{}_{}.csv".format(
             int(time.time()), self.__class__.__name__, self.name))
+
 
 class FileSensor(Sensor):
     def __init__(self, file_ext, *args, **kwargs):
@@ -54,6 +58,7 @@ class Random(LogSensor):
 
         with open(self.file_path, "a") as file:
             file.write("{},{}\n".format(ts, random.randint(0, self.maximum)))
+
 
 @_register_sensor
 class RandomFile(FileSensor):
@@ -79,6 +84,28 @@ class AM2302(LogSensor):
 
         with open(self.file_path, "a") as file:
             file.write("{},{},{}\n".format(ts, humidity, temp))
+
+
+@_register_sensor
+class Microphone(FileSensor):
+    def __init__(self, *args, device_name="hw:1,0", file_type="wav",
+                 duration_sec=30, format="S16_LE", rate=44100, **kwargs):
+        super().__init__(file_type, *args, **kwargs)
+        self.device_name = device_name
+        self.file_type = file_type
+        self.duration_sec = duration_sec
+        self.format = format
+        self.rate = rate
+
+    def read(self):
+        subprocess.run([
+            "arecord",
+            "-D", self.device_name,
+            "-t", self.file_type,
+            "-d", str(self.duration_sec),
+            "-f", self.format,
+            "-r", str(self.rate),
+            self.file_path])
 
 
 # import picamera
