406450eaabfc8b88b89a2915fc78af1f652fb5a2
Jonas Höchst <git@jonashoechst.de>
2019-02-19 16:55:42 +0100

    removed python 2 support

--- setup.py
+++ setup.py
@@ -31,8 +31,6 @@ setup(
     classifiers=[
         "Development Status :: 4 - Beta",
         "License :: OSI Approved :: MIT License",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
