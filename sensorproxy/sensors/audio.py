a2b93abac96ee8531abf8c2404fe51d4edd9fa77
Jonas Höchst <git@jonashoechst.de>
2019-02-21 17:02:34 +0100

    removed unused value

--- sensorproxy/sensors/audio.py
+++ sensorproxy/sensors/audio.py
@@ -10,7 +10,7 @@ log = logging.getLogger("pysensorproxy.sensors.audio")
 @register_sensor
 class Microphone(FileSensor):
     def __init__(self, *args, device_name="hw:1,0", file_type="wav",
-                 duration_sec=30, format="S16_LE", rate=44100, **kwargs):
+                 format="S16_LE", rate=44100, **kwargs):
         super().__init__(file_type, *args, **kwargs)
 
         self.device_name = device_name
