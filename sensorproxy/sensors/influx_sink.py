b398d6c0a177c76e14d11ea4c5d7da878f83d34d
Alvar Penning <post@0x21.biz>
2019-05-06 18:40:39 +0200

    InfluxSink uses global influx configuration

--- sensorproxy/sensors/influx_sink.py
+++ sensorproxy/sensors/influx_sink.py
@@ -9,16 +9,20 @@ logger = logging.getLogger(__name__)
 
 @register_sensor
 class InfluxSink(Sensor):
-    def __init__(self, *args, host, port, user, password, db, path, directory, ssl, **kwargs):
-        super().__init__(*args, **kwargs)
+    """ InfluxSink consumes all csv files from a given directory and publishes
+        their content to an InfluxDB. Therefore a global `influx` configuration
+        must exist inside of `sensorproxy.yml`.
+    """
 
-        self._influx = InfluxAPI(host, port, user, password, db, path, ssl)
+    def __init__(self, *args, directory, **kwargs):
+        super().__init__(*args, **kwargs)
         self._directory = directory
 
-        logger.info("Started Influx on {}".format(self._influx.client._baseurl))
+    def record(self, *args, dry: bool = False, influx: InfluxAPI = None, **kwargs):
+        if influx is None:
+            raise SensorNotAvailableException(
+                    "InfluxSink received an empty `influx` object")
 
-    def record(self, *args, dry: bool = False, height_m: float = None, **kwargs):
-        logger.info("Recording InfluxSink..")
         files = [os.path.join(self._directory, f)
                  for f in os.listdir(self._directory)
                  if f.endswith('.csv')]
@@ -26,10 +30,10 @@ class InfluxSink(Sensor):
         for f in files:
             logger.info("Sending {} to InfluxDB".format(f))
             if not dry:
-                self._influx.submit_file(f, "test1")
+                influx.submit_file(f, "test1")  # TODO: sensor's name
 
     def refresh(self):
-        logger.info("Refreshing InfluxSink..")
+        pass
 
     def get_file_path(self):
-        logger.info("Fetching FilePath for InfluxSink..")
+        return None
